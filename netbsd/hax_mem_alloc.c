1542f16cfc0d0c0c07b56b04619ab54d2506c018
Kamil Rytarowski <n54@gmx.com>
2017-11-21 08:43:53 +0100

    netbsd/hax_mem_alloc.c: Initial porting to NetBSD
    Work in progress.

--- netbsd/hax_mem_alloc.c
+++ netbsd/hax_mem_alloc.c
@@ -30,8 +30,10 @@
 
 #include <sys/param.h>
 #include <sys/types.h>
-#include <sys/malloc.h>
-#include <sys/kmem.h>
+#include <sys/mutex.h>
+#include <sys/rbtree.h>
+#include <sys/systm.h>
+#include <uvm/uvm.h>
 
 #include "com_intel_hax.h"
 
@@ -48,6 +50,99 @@
 
 #define HAX_CACHE_ALIGNMENT 0x10
 
+/* We must cache flags passed to uvm_km_alloc(9) */
+struct vmalloc_pair_entry {
+    void      *first;  /* va */
+    uvm_flag_t second; /* flags */
+    rb_node_t pair_node;
+};
+
+static int
+vmalloc_compare_key(void *ctx, const void *n1, const void *keyp)
+{
+    const void *a1;
+    const void *a2;
+
+    assert(a1);
+    assert(keyp);
+
+    a1 = ((struct plist_pair_entry*)n1)->first;
+    a2 = (const void *)keyp;
+
+    return a1 > a2;
+}
+
+static int
+vmalloc_compare_node(void *ctx, const void *n1, const void *n2)
+{
+    const void *key2;
+
+    assert(n1);
+    assert(n2);
+    assert(((struct vmalloc_pair_entry*)n2)->first);
+
+    key2 = ((struct vmalloc_pair_entry*)n2)->first;
+
+    return vmalloc_compare_key(ctx, n1, key2);
+}
+
+static const rb_tree_ops_t vmalloc_tree_ops = {
+    .rbto_compare_nodes = vmalloc_compare_nodes,
+    .rbto_compare_key = vmalloc_compare_key,
+    .rbto_node_offset = offsetof(struct vmalloc_pair_entry, pair_node),
+    .rbto_context = NULL
+};
+
+rb_tree_t vmalloc_tree;
+kmutex_t vmalloc_tree_mut;
+
+static void
+vmalloc_tree_insert(const void *va, const uvm_flag_t flags)
+{
+    struct vmalloc_pair_entry *pair, *opair;
+
+    assert(va);
+
+    pair = kmem_alloc(sizeof(*pair), KM_SLEEP);
+    if (pair == NULL)
+        panic("kmem_alloc failed\n");
+
+    pair->first = va;
+    pair->second = flags;
+
+    mutex_enter(&vmalloc_tree_mut);
+    opair = rb_tree_insert_node(&vmalloc_tree, pair);
+    mutex_exit(&vmalloc_tree_mut);
+
+    if (opair != pair)
+        panic("Attempted to register duplicate entry key=%p value=%llx "
+              "(okey=%p ovalue=%llx)\n",
+              pair->first, pair->second, opair->first, opair->second);
+}
+
+uvm_flag_t
+vmalloc_tree_retrieve(const void *va)
+{
+    struct vmalloc_pair_entry *pair;
+    uvm_flag_t flags;
+
+    mutex_enter(&vmalloc_tree_mut);
+    pair = rb_tree_find_node(&vmalloc_tree, va);
+    if (pair == NULL)
+        panic("Attempted to register duplicate entry key=%p value=%llx "
+              "(okey=%p ovalue=%llx)\n",  
+              pair->first, pair->second, opair->first, opair->second);
+
+    rb_tree_remove_node(&vmalloc_tree, pair);
+    mutex_exit(&vmalloc_tree_mut);
+
+    flags = pair->second;
+
+    kmem_free(pair, sizeof(*pair));
+
+    return flags;
+}
+
 /* XXX init to be 0? */
 void * hax_vmalloc(uint32_t size, uint32_t flags)
 {
@@ -60,59 +155,50 @@ void * hax_vmalloc(uint32_t size, uint32_t flags)
         return NULL;
 
     if (flags & HAX_MEM_PAGABLE)
-        buf = malloc(size, HAX_CACHE_ALIGNMENT);
+        buf = uvm_km_alloc(kernel_map, size, 0, UVM_KMF_PAGEABLE | UVM_KMF_ZERO);
 
     if (flags & HAX_MEM_NONPAGE)
-        buf = kmem_zalloc(size, KM_SLEEP);
-
-    if (buf)
-        memset(buf, 0, size);
+        buf = uvm_km_alloc(kernel_map, size, 0, UVM_KMF_WIRED | UVM_KMF_ZERO);
 
     return buf;
 }
 
-extern "C" void * hax_vmalloc_aligned(uint32_t size, uint32_t flags,
+void * hax_vmalloc_aligned(uint32_t size, uint32_t flags,
                                       uint32_t alignment)
 {
     void *buf = NULL;
-    HAX_ALLOC_CHECK
+    HAX_ALLOC_CHECK();
 
     if (flags & HAX_MEM_PAGABLE)
-        buf = IOMallocPageable(size, alignment);
+        buf = uvm_km_alloc(kernel_map, size, alignment, UVM_KMF_PAGEABLE | UVM_KMF_ZERO);
 
     if (flags & HAX_MEM_NONPAGE)
-        buf = IOMallocAligned(size, alignment);
-
-    if (buf)
-        memset(buf, 0, size);
+        buf = uvm_km_alloc(kernel_map, size, alignment, UVM_KMF_WIRED | UVM_KMF_ZERO);
 
     return buf;
 }
 
 #undef HAX_ALLOC_CHECK_FAIL
 #define HAX_ALLOC_CHECK_FAIL
-extern "C" void hax_vfree_flags(void *va, uint32_t size, uint32_t flags)
+
+void hax_vfree_flags(void *va, uint32_t size, uint32_t flags)
 {
-    HAX_ALLOC_CHECK
+    HAX_ALLOC_CHECK();
 
-    if (flags & HAX_MEM_PAGABLE)
-        return IOFreePageable(va, size);
-
-    if (flags & HAX_MEM_NONPAGE)
-        return IOFree(va, size);
+    uvm_km_free(kernel_map, va, size, flags);
 }
 
-extern "C" void hax_vfree(void *va, uint32_t size)
+void hax_vfree(void *va, uint32_t size)
 {
     uint32_t flags = HAX_MEM_NONPAGE;
 
     hax_vfree_flags(va, size, flags);
 }
 
-extern "C" void hax_vfree_aligned(void *va, uint32_t size, uint32_t alignment,
+void hax_vfree_aligned(void *va, uint32_t size, uint32_t alignment,
                                   uint32_t flags)
 {
-    HAX_ALLOC_CHECK
+    HAX_ALLOC_CHECK();
 
     if (flags & HAX_MEM_PAGABLE) {
         IOFreePageable(va, size);
@@ -133,7 +219,7 @@ struct _hax_vmap_entry {
     uint32_t size;
 };
 
-extern "C" void * hax_vmap(hax_pa_t pa, uint32_t size)
+void * hax_vmap(hax_pa_t pa, uint32_t size)
 {
     IOMemoryDescriptor *md;
     IOMemoryMap *mm;
@@ -170,7 +256,7 @@ extern "C" void * hax_vmap(hax_pa_t pa, uint32_t size)
     return entry->va;
 }
 
-extern "C" void hax_vunmap(void *addr, uint32_t size)
+void hax_vunmap(void *addr, uint32_t size)
 {
     unsigned long va = (unsigned long)addr;
     struct _hax_vmap_entry *entry, *n;
@@ -195,7 +281,7 @@ extern "C" void hax_vunmap(void *addr, uint32_t size)
     printf("Failed to find the virtual address %lx\n", va);
 }
 
-extern "C" hax_pa_t hax_pa(void *va)
+hax_pa_t hax_pa(void *va)
 {
     uint64_t pa;
     struct IOMemoryDescriptor *bmd;
@@ -223,7 +309,7 @@ extern "C" hax_pa_t hax_pa(void *va)
  * map it. This should be acceptable considering the 4G kernel space even on
  * 32-bit kernel.
  */
-extern "C" struct hax_page * hax_alloc_pages(int order, uint32_t flags,
+struct hax_page * hax_alloc_pages(int order, uint32_t flags,
                                              bool vmap)
 {
     struct hax_page *ppage = NULL;
@@ -262,7 +348,7 @@ error:
     return NULL;
 }
 
-extern "C" void hax_free_pages(struct hax_page *pages)
+void hax_free_pages(struct hax_page *pages)
 {
     if (!pages)
         return;
@@ -279,36 +365,36 @@ extern "C" void hax_free_pages(struct hax_page *pages)
     hax_vfree(pages, sizeof(struct hax_page));
 }
 
-extern "C" void * hax_map_page(struct hax_page *page)
+void * hax_map_page(struct hax_page *page)
 {
     return page->kva;
 }
 
 /* On Mac, it is always mapped */
-extern "C" void hax_unmap_page(struct hax_page *page)
+void hax_unmap_page(struct hax_page *page)
 {
     return;
 }
 
-extern "C" hax_pfn_t hax_page2pfn(struct hax_page *page)
+hax_pfn_t hax_page2pfn(struct hax_page *page)
 {
     if (!page)
         return 0;
     return page->pa >> page_shift;
 }
 
-extern "C" void hax_clear_page(struct hax_page *page)
+void hax_clear_page(struct hax_page *page)
 {
     memset((void *)page->kva, 0, 1 << page_shift);
 }
 
-extern "C" void hax_set_page(struct hax_page *page)
+void hax_set_page(struct hax_page *page)
 {
     memset((void *)page->kva, 0xff, 1 << page_shift);
 }
 
 /* Initialize memory allocation related structures */
-extern "C" int hax_malloc_init(void)
+int hax_malloc_init(void)
 {
     /* vmap related */
     hax_init_list_head(&_vmap_list);
@@ -318,10 +404,15 @@ extern "C" int hax_malloc_init(void)
         return -ENOMEM;
     }
 
+    rb_tree_init(&vmalloc_tree, &vmalloc_tree_ops);
+    mutex_init(&vmalloc_tree_mut, MUTEX_DEFAULT, IPL_NONE);
+
     return 0;
 }
 
-extern "C" void hax_malloc_exit(void)
+void hax_malloc_exit(void)
 {
     hax_spinlock_free(vmap_lock);
+
+    mutex_destroy(&vmalloc_tree_mut);
 }
