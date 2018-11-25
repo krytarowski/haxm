32bf73a71e80cdd0bb350d654b3ea057ac59f689
Kamil Rytarowski <n54@gmx.com>
2018-11-25 08:20:21 +0100

    fix

--- platforms/netbsd/hax_entry.c
+++ platforms/netbsd/hax_entry.c
@@ -367,6 +367,7 @@ init_err1:
         config_cfdata_detach(hax_vm_cfdata);
         config_cfattach_detach(hax_vm_cd.cd_name, &hax_vm_ca);
         config_cfdriver_detach(&hax_vm_cd);
+
         hax_info("Removed HAXM device\n");
         return 0;
     }
