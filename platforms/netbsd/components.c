2968325687c6b9532754d47db9f93466846b241b
Kamil Rytarowski <n54@gmx.com>
2018-11-26 00:53:29 +0100

    fix

--- platforms/netbsd/components.c
+++ platforms/netbsd/components.c
@@ -111,7 +111,7 @@ int hax_vcpu_destroy_host(struct vcpu_t *cvcpu, void *vcpu_host)
 
     vcpu = (hax_vcpu_netbsd_t *)vcpu_host;
 
-    minor = vcpu->vm->id * HAX_MAX_VMS + vcpu->id;
+    minor = vcpu->vm->id * HAX_MAX_VCPUS + vcpu->id;
     sc = device_lookup_private(&hax_vcpu_cd, minor);
     if (!sc) {
         hax_error("device lookup for hax_vcpu failed (minor %u)\n", minor);
